f1f38a2f87eca2231e30a51603f348b71d4d8642
Anurag Kumbhare <anurag@MacBook-Air.local>
2025-03-25 01:28:38 +0530

    code updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import obfuscator from 'rollup-plugin-javascript-obfuscator';
-import replace from '@rollup/plugin-replace';
+import obfuscator from 'rollup-plugin-javascript-obfuscator'
+import replace from '@rollup/plugin-replace'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/gcd-demo-React/',
   plugins: [
     react(),
     replace({
@@ -13,33 +11,19 @@ export default defineConfig({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
     obfuscator({
-    compact: true,
-    controlFlowFlattening: true,
-    controlFlowFlatteningThreshold: 0.75,
-    deadCodeInjection: true,
-    deadCodeInjectionThreshold: 0.4,
-    debugProtection: true,
-    debugProtectionInterval: true,
-    disableConsoleOutput: true,
-    identifierNamesGenerator: 'hexadecimal',
-    rotateStringArray: true,
-    selfDefending: true,
-    stringArray: true,
-    stringArrayEncoding: ['base64'],
-    stringArrayThreshold: 0.75,
-    transformObjectKeys: true,
-    unicodeEscapeSequence: false
-  })
-],
-build: {
-  sourcemap: false,
-  minify: 'terser',
-  rollupOptions: {
-    output: {
-      manualChunks: {
-        vendor: ['react', 'react-dom', 'react-router-dom']
+      compact: true,
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 0.75,
+      deadCodeInjection: true,
+    })
+  ],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
       }
     }
   }
-}
-});
+})
