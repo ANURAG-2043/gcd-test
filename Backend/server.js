68e39e64673e05df1d5a87c16ef77d1e7993dfd0
Anurag Kumbhare <anurag@MacBook-Air.local>
2025-03-25 14:51:08 +0530

    deployment changes

--- Backend/server.js
+++ Backend/server.js
@@ -8,8 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: ['https://gcd-demo-react.vercel.app', 'http://localhost:5173', 'https://geocitydatabase-final.onrender.com'],
-    credentials: true
+    origin: [
+        'https://gcd-test-o1rb8sp98-anurag2043s-projects.vercel.app',
+        'http://localhost:5173'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use('/api/users', userRoutes);  // Added forward slash before 'api'
